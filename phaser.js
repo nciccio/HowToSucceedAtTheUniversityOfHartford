4d49a78d6935f10c5e77e9f6b5e3d3fb8625405f
mattWStevens <42496928+mattWStevens@users.noreply.github.com>
2019-04-03 17:10:47 -0400

    Update phaser.js

--- phaser.js
+++ phaser.js
@@ -1,7 +1,7 @@
 var config = {
     type: Phaser.AUTO,
-    width: Window.width,
-    height: Window.height,
+    width: 800,
+    height: 600,
     physics: {
         default: 'arcade',
         arcade: {
