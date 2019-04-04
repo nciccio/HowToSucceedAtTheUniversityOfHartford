e321b9327dfe8d2fa761d9e8f52571b8e2106733
mattWStevens <42496928+mattWStevens@users.noreply.github.com>
2019-04-04 14:28:17 -0400

    Update phaser.js

--- phaser.js
+++ phaser.js
@@ -21,7 +21,7 @@ var cursors;
 function preload()
 {
     this.load.image('background', 'tilesSet.PNG');
-    this.load.spritesheet('student', 'student_real.png', {frameWidth: 57, frameHeight: 99 });
+    this.load.spritesheet('student', 'real_student.png', {frameWidth: 57, frameHeight: 99 });
 }
 
 function create()
