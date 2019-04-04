7e74bd4e49ca1fe0b7d69ffbe7e289e7abafdb38
imoise <43978759+imoise@users.noreply.github.com>
2019-04-04 16:21:00 -0400

    Update phaser.js

--- phaser.js
+++ phaser.js
@@ -21,7 +21,7 @@ var cursors;
 function preload()
 {
     this.load.image('background', 'tilesSet.PNG');
-    this.load.spritesheet('student', 'real_student.png', {frameWidth: 51.5, frameHeight: 99 });
+    this.load.spritesheet('student', 'real_student.png', {frameWidth: 51.7, frameHeight: 99 });
 }
 
 function create()
