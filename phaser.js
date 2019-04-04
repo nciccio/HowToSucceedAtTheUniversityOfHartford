0c7765ff2e016b3d813c2282afcc0e0ec027948d
mattWStevens <42496928+mattWStevens@users.noreply.github.com>
2019-04-04 14:19:40 -0400

    Update phaser.js

--- phaser.js
+++ phaser.js
@@ -33,7 +33,7 @@ function create()
     
     this.anims.create({
         key: 'left',
-        frames: this.anims.generateFrameNumbers('student', {start:4, end:7}),
+        frames: this.anims.generateFrameNumbers('student', {start:5, end:7}),
         frameRate: 10,
         repeat: -1
     });
