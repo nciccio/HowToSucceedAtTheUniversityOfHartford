dd87f7e6498e32903efd6448fe8c047854bad871
mattWStevens <42496928+mattWStevens@users.noreply.github.com>
2019-04-07 14:03:45 -0400

    Add files via upload

---
+++ classes.js
@@ -0,0 +1,52 @@
+class Timer {
+     constructor(time) {
+        this.minutes = time;
+        this.seconds = 0;
+    }
+
+    getTime() {
+        return this.seconds < 10 ? 
+        (this.minutes + ":0" + this.seconds) : 
+        (this.minutes + ":" + this.seconds);
+    }
+
+    getMinutes() {
+        return this.minutes;
+    }
+
+    getSeconds() {
+        return this.seconds;
+    }
+
+    setMinutes(time) {
+        this.minutes = time;
+    } 
+}
+
+function countDown() {
+    var display = document.getElementById('time');
+
+    var secondsDisplay = seconds < 10 ? "0" + seconds : seconds;
+
+    display.innerHTML = minutes + ":" + secondsDisplay;
+
+    seconds--;
+
+    if (seconds <= 0) {
+        minutes--;
+        seconds = 59;
+    }
+}
+
+var masterTime = new Timer(4);
+
+function loadPage() {
+    seconds = masterTime.getSeconds();
+    minutes = masterTime.getMinutes();
+
+    var counter = setInterval(countDown, 1000);
+
+    if (minutes == 0) {
+        clearInterval(counter);
+    }
+}
