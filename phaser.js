b3b25c3856b4e7bb9ab4b50b37619e8fb519957a
imoise <43978759+imoise@users.noreply.github.com>
2019-04-09 15:53:31 -0400

    Update phaser.js

--- phaser.js
+++ phaser.js
@@ -72,11 +72,20 @@ function create()
         repeat: -1
     });
     function quiz(){
-    var quiz = new Phasetips(_game, {
-    context: "Hello tooltip",
-    strokeColor: 0xff0000
+    var quiz = new Phasetips{_game, {
+    targetObject: block1,
+    context: "A cute bunny but with a lot of text, that needs to wrap!",
+    strokeColor: 0xff0000,
+    position: "right"
   });
 }
+    
+  var tip1 = new Phasetips(_game, {
+    targetObject: student,
+    context: "A cute bunny but with a lot of text, that needs to wrap!",
+    strokeColor: 0xff0000,
+    position: "right",
+
     this.physics.add.collider(player, desks, quiz(), null, this);
     cursors = this.input.keyboard.createCursorKeys();
 }
